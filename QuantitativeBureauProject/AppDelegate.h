a6e21fac55bd4c972fda10255c454ed52d20afc1
杨晓婧 <yangxiaojing@yangxiaojingmatomacbook-pro.local>
2016-03-17 08:27:18 +0800

    Initial Commit

---
+++ QuantitativeBureauProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuantitativeBureauProject
+//
+//  Created by 杨晓婧 on 16/3/17.
+//  Copyright © 2016年 shangtong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuantitativeBureauProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuantitativeBureauProject
+//
+//  Created by 杨晓婧 on 16/3/17.
+//  Copyright © 2016年 shangtong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
